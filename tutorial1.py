45eac445395af509099699ca9ffbd64e0bbf1da2
nnfuad <nnfuad01@gmail.com>
2025-07-21 22:31:14 +0600

    added tutorial1.py

--- tutorial1.py
+++ tutorial1.py
@@ -6,7 +6,7 @@ print(cv2.__version__)
 img = cv2.imread('assets/centerdiv.jpg', -1)
 img = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
 img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
-
+cv2.imwrite("new_img.jpg", img)
 
 cv2.imshow('Image', img)
 cv2.waitKey(10000)
@@ -15,4 +15,5 @@ cv2.destroyAllWindows()
 	# Read the image
 	# Resize it to 50%
 	# Rotate it 90° counter-clockwise
+    # Saves it to newfile.
 	# Show it in a window for 10 seconds
